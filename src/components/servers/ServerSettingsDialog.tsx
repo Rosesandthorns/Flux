8de34c817988bfccc78f53e7c40fb26e4e6098bb
Roses And Thorns <powwerofpowwer@gmail.com>
2026-02-11 20:27:43 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/servers/ServerSettingsDialog.tsx
+++ src/components/servers/ServerSettingsDialog.tsx
@@ -1,4 +1,5 @@
 
+
 'use client';
 
 import { useEffect, useMemo, useState } from 'react';
@@ -25,7 +26,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch';
+import { Switch } from "@/components/ui/switch";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { useToast } from "@/hooks/use-toast";
