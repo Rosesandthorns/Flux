0006e4c618110ce1e32fe79cf2bb396b2a5a47f3
Roses And Thorns <powwerofpowwer@gmail.com>
2026-02-12 00:01:15 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://s1.ezgi

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's1.ezgif.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
